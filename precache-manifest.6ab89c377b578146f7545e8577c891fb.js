e3132fcd33de259d1d99472126e017542b215afa
caio nakai <kaioesperto@hotmail.com>
2020-07-25 15:00:02 +0100

    Updates

---
+++ precache-manifest.6ab89c377b578146f7545e8577c891fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840cf1ec3b62b7e94515127daff13af7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d81cdc2de0dd97cd725",
+    "url": "/static/css/main.73195b8f.chunk.css"
+  },
+  {
+    "revision": "41af9a5d408abca761b8",
+    "url": "/static/js/2.614923d2.chunk.js"
+  },
+  {
+    "revision": "9a8bb2b00f7c689a2b26d7b5cc824834",
+    "url": "/static/js/2.614923d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d81cdc2de0dd97cd725",
+    "url": "/static/js/main.7f455c55.chunk.js"
+  },
+  {
+    "revision": "1b23714ce927fcd73fab",
+    "url": "/static/js/runtime-main.d9d9a5b0.js"
+  },
+  {
+    "revision": "0375009b23d5cabce196f4499dddeb34",
+    "url": "/static/media/code.0375009b.jpg"
+  },
+  {
+    "revision": "859120e5f7242a97c9c06b9f6c23571a",
+    "url": "/static/media/cv.859120e5.pdf"
+  },
+  {
+    "revision": "216c8ded97e9f3e5caa53e0c24082262",
+    "url": "/static/media/educast.216c8ded.png"
+  },
+  {
+    "revision": "f45e79f3a5e843dfbbb69aeec834202c",
+    "url": "/static/media/funny_calculus.f45e79f3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5f1d4d25ac205bd597e2e9c1b53d3e1",
+    "url": "/static/media/space_management.f5f1d4d2.png"
+  }
+]);
